029a64c1229b0a8fc657498a52dfb652716c2f07
ferris <jake@outracks.com>
2014-05-07 13:51:19 +0200

    this is gonna be so rad

--- GamesPreprocessor/Program.fs
+++ GamesPreprocessor/Program.fs
@@ -10,57 +10,58 @@ type Game =
     
 [<EntryPoint>]
 let main argv =
-    let gamesDirName = argv.[0]
-    let outputFileName = argv.[1]
+    let loadGames (dirName : string) =
+        let isGameFileName (x : string) = x.EndsWith "ch8"
+        let isInfoFileName (x : string) = x.EndsWith "txt"
 
-    let allFileNames = Directory.GetFiles gamesDirName
+        let allFileNames = Directory.GetFiles dirName
+        let gameFileNames = Array.filter isGameFileName allFileNames
+        let infoFileNames = Array.filter isInfoFileName allFileNames
 
-    let gameExtension = "ch8"
-    let infoExtension = "txt"
+        let loadGame (fileName : string) =
+            let removeTag (s : string) (t : string option) =
+                match t with
+                | Some x -> (s.Replace (x, "")).Trim()
+                | _ -> s
 
-    let isGameFileName (x : string) = x.EndsWith gameExtension
-    let isInfoFileName (x : string) = x.EndsWith infoExtension
+            let getAuthorTag (s : string) =
+                match s.Contains "[" && s.Contains "]" with
+                | true ->
+                    let tagStart = s.IndexOf '[' - 1
+                    let tagEnd = s.IndexOf ']' + 1
+                    Some (s.Substring (tagStart, tagEnd - tagStart))
+                | _ -> None
 
-    let gameFileNames = Array.filter isGameFileName allFileNames
-    let infoFileNames = Array.filter isInfoFileName allFileNames
+            let removeAuthorTag s = s |> getAuthorTag |> removeTag s
 
-    let getGame (fileName : string) =
-        let removeTag (s : string) (t : string option) =
-            match t with
-            | Some x -> (s.Replace (x, "")).Trim()
-            | _ -> s
+            let getTitle s = removeAuthorTag (Path.GetFileNameWithoutExtension s)
 
-        let getAuthorTag (s : string) =
-            match s.Contains "[" && s.Contains "]" with
-            | true ->
-                let tagStart = s.IndexOf '[' - 1
-                let tagEnd = s.IndexOf ']' + 1
-                Some (s.Substring (tagStart, tagEnd - tagStart))
-            | _ -> None
+            let getAuthor s =
+                match getAuthorTag s with
+                | Some x -> Some (x.Substring (2, x.Length - 3))
+                | _ -> None
 
-        let removeAuthorTag (s : string) = s |> getAuthorTag |> removeTag s
+            let getInfo s =
+                let title = getTitle s
+                match Array.tryFind (fun (x : string) -> x.Contains title) infoFileNames with
+                | Some x -> Some (File.ReadAllText x)
+                | _ -> None
 
-        let getTitle (s : string) = removeAuthorTag (Path.GetFileNameWithoutExtension s)
+            {
+                Name = getTitle fileName;
+                Author = getAuthor fileName;
+                Info = getInfo fileName;
+                Data = File.ReadAllBytes fileName;
+            }
 
-        let getAuthor (s : string) =
-            match getAuthorTag s with
-            | Some x -> Some (x.Substring (2, x.Length - 3))
-            | _ -> None
+        Array.map loadGame gamefileNames
 
-        let getInfo (s : string) =
-            let title = getTitle s
-            match Array.tryFind (fun (x : string) -> x.Contains title) infoFileNames with
-            | Some x -> Some (File.ReadAllText x)
-            | _ -> None
+    let saveGames (games : Game array) (fileName : string) =
+        use w = new StreamWriter(fileName)
 
-        {
-            Name = getTitle fileName;
-            Author = getAuthor fileName;
-            Info = getInfo fileName;
-            Data = File.ReadAllBytes fileName;
-        }
-        
-    let games = Array.map getGame gameFileNames
-    printfn "%A" games
+        let line s = w.WriteLine s
+
+    let games = loadGames argv.[0]
+    saveGames games argv.[1]
 
     0
