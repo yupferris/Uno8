caf27afd584a7dabc7153c096092525c948cda11
ferris <jake@outracks.com>
2014-05-08 11:11:47 +0200

    sweeeeeeeet

--- GamesPreprocessor/Program.fs
+++ GamesPreprocessor/Program.fs
@@ -64,7 +64,7 @@ let main argv =
         let line s =
             let prefix =
                 match !indent with
-                | n when n > 0 -> Array.reduce (fun x y -> x + y) (Array.create !indent "\t")
+                | n when n > 0 -> Array.reduce (+) (Array.create !indent "\t")
                 | _ -> ""
             w.WriteLine (prefix + s)
 
@@ -105,7 +105,7 @@ let main argv =
 
                 line
                     (Array.reduce
-                        (fun x y -> x + y)
+                        (+)
 
                         [|for i in 0 .. lineLength - 1 ->
                             match startIndex + i with
